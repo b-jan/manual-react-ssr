77f63daedbd48f35dedfafec0828a961771e60b5
b-jan <bapt.jan@gmail.com>
2018-04-01 18:14:57 +0200

    Implement SSR on create-react-app

---
+++ server/middleware/renderer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import App from '../../src/App';
+
+const path = require("path");
+const fs = require("fs");
+
+export default (req, res, next) => {
+  const filePath = path.resolve(__dirname, '..', '..', 'build', 'index.html');
+
+  fs.readFile(filePath, 'utf8', (err, htmlData) => {
+    if (err) {
+      console.error('err', err);
+      return res.status(404).end()
+    }
+    const html = ReactDOMServer.renderToString(<App />);
+    return res.send(
+      htmlData.replace(
+        '<div id="root"></div>',
+        `<div id="root">${html}</div>`
+      )
+    );
+  });
+}
